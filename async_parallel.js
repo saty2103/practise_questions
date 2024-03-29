efecd4a3831bdab7f539371ac9aeb1546994718f
SATYAM CHATTERJEE <satyamchatterjee@192.168.1.5>
2024-03-30 01:16:29 +0530

    feat: async tasks in parallel
    In this commit, we are pushing a code which first initializes 5 parallel asynchronus tasks. Only after the all the asynchronus tasks are completed, it invokes a callback function

---
+++ async_parallel.js
@@ -0,0 +1,51 @@
+function createAsyncTask() {
+  const value = Math.floor(Math.random() * 10);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value < 5) {
+        reject(`Error ${value}`);
+      } else {
+        resolve(value * 1000);
+      }
+    }, value * 1000);
+  });
+}
+
+let tasks = [
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+];
+
+function asyncParallel(tasks, callback) {
+  let error = [];
+  let result = [];
+  let completed = 0;
+
+  tasks.forEach((task, index) =>
+    task
+      .then((resp) => {
+        result.push(resp);
+        console.log(resp, index);
+      })
+      .catch((err) => {
+        error.push(err);
+        console.log(err, index);
+      })
+      .finally(() => {
+        completed++;
+
+        if (completed >= tasks.length) {
+          callback(error, result);
+        }
+      })
+  );
+}
+
+asyncParallel(tasks, (error, result) => {
+  console.error("error ->", error);
+  console.log("result ->", result);
+});
